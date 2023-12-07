a8f3b0246969133fe2544c0563a2da4a76980aea
Debasishkaity22 <139854092+Debasishkaity22@users.noreply.github.com>
2023-12-07 15:07:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function daysOfAYear(year){
+	let leapYear=((year%4===0&&year%100!==0)||(year%400===0));
+	if(leapYear===true){
+		return 366;
+	}
+	else{
+		return 365;
+	}
+	
+}
+let year=prompt("Enter the Year");
+alert("no of days:"+daysOfAYear(year));
